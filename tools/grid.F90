7a1528194790aaffd9a925ffd6660ff1d58c37c9
Casper Steinmann <casper.steinmann@gmail.com>
2014-10-08 23:59:16 +0200

    Simplified the GRID tool significantly

--- tools/grid.F90
+++ tools/grid.F90
@@ -7,42 +7,22 @@ program test
     implicit none
 
     real(dp), dimension(:,:), allocatable :: wrk
-    integer :: nat
-    character(len=80) :: option
-    character(len=2) :: atom
-    real(dp) :: factor = 1.0_dp
-    real(dp), dimension(:,:), allocatable :: R
-    real(dp), dimension(:), allocatable :: Z
+    real(dp), dimension(3,3) :: R
+    real(dp), dimension(3) :: Z
     integer :: ntotalpoints, ntruepoints, k
-    integer :: ifile
 
-qfit_nshell = 1
-qfit_pointdensity = 0.28_dp
-call openfile("input.xyz", ifile, 'old', 'formatted')
-rewind(ifile)
-read(ifile,*) nat
-read(ifile,*) option
-if(trim(option) == 'AA') then
-    factor = aa2au
-endif
-allocate(R(3,nat))
-allocate(Z(nat))
-do k=1,nat
-    read(ifile,*) atom, R(:,k)
-    if (trim(atom) == 'H') Z(k) = 1.0_dp
-    if (trim(atom) == 'C') Z(k) = 6.0_dp
-    if (trim(atom) == 'N') Z(k) = 7.0_dp
-    if (trim(atom) == 'O') Z(k) = 8.0_dp
-    if (trim(atom) == 'F') Z(k) = 9.0_dp
-    if (trim(atom) == 'P') Z(k) = 15.0_dp
-    if (trim(atom) == 'S') Z(k) = 16.0_dp
-enddo
-close(ifile)
+R(:,1) = (/0.0, 0.0, 0.0/)
+R(:,2) = (/0.9, 0.0, 0.0/)
+R(:,3) = (/-0.41, 0.81, 0.0/)
+Z = (/8.0, 1.0, 1.0/)
 
-R = R * factor
+R = R * aa2au
+
+qfit_nshell = 4
 
 call connolly_initialize( R, Z )
 call connolly_grid_count
+
 
 ntotalpoints = sum(max_layer_points)
 allocate( wrk( 3, ntotalpoints ) )
@@ -52,45 +32,11 @@ write(*,'(i4)') ntruepoints
 write(*,*)
 
 do k = 1, ntruepoints
-    write(*,'(a,f20.9,2f16.9)') 'X', wrk(:,k)
+    write(*,'(a,f20.9,2f16.9)') 'X', wrk(:,k) / aa2au
 enddo
 
 call connolly_finalize
 
-deallocate( wrk )
-deallocate( R )
-deallocate( Z )
-
 
 end program test
 
-subroutine openfile(filename, lunit, stat, frmt)
-
-    character(*), intent(in) :: filename, stat, frmt
-    integer, intent(out) :: lunit
-    integer :: i
-    logical :: lexist, lopen
-
-    if (stat == 'old') then
-        inquire(file=filename, exist=lexist)
-
-        if (.not. lexist) then
-            print *, filename, ' not found!'
-            stop
-        end if
-    end if
-
-    do i = 21, 99
-        inquire(unit=i, opened=lopen)
-        if (lopen) then
-            cycle
-        else
-            lunit = i
-            open(unit=lunit, file=filename, status=stat, form=frmt)
-            exit
-        end if
-    end do
-
-    return
-
-end subroutine openfile
